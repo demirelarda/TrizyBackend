32bcea30829ef7ab06dec985c2f25debdb9fff29
arda <yademirel21@gmail.com>
2024-12-12 17:19:47 +0300

    Added auth routes

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
+const authRoute = require('./routes/auth')
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to the MongoDB")).catch((err) => { console.log(err) })
 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/api', authRoute)
 
 
 const PORT = process.env.PORT || 3000
