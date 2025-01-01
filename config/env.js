33a83059ba5d55bbcac27d7505d242b0515ac635
arda <yademirel21@gmail.com>
2025-01-01 23:50:12 +0300

    Updated redis client

--- config/env.js
+++ config/env.js
@@ -3,9 +3,11 @@ require('dotenv').config()
 const USE_REDIS = process.env.USE_REDIS === 'true'
 const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'
 const REDIS_PORT = parseInt(process.env.REDIS_PORT || '6379', 10)
+const REDIS_PASSWORD = process.env.REDISPASSWORD || null
 
 module.exports = {
   USE_REDIS,
   REDIS_HOST,
-  REDIS_PORT
+  REDIS_PORT,
+  REDIS_PASSWORD
 }
--- services/redisClient.js
+++ services/redisClient.js
@@ -1,5 +1,5 @@
 const Redis = require('ioredis')
-const { REDIS_HOST, REDIS_PORT } = require('../config/env')
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = require('../config/env')
 
 let redis = null
 
@@ -7,11 +7,12 @@ function createRedisClient() {
   if (!redis) {
     redis = new Redis({
       host: REDIS_HOST,
-      port: REDIS_PORT
+      port: REDIS_PORT,
+      password: REDIS_PASSWORD
     })
 
     redis.on('connect', () => {
-      console.log('[Redis] Connected to Redis server')
+      console.log(`[Redis] Connected to Redis server at ${REDIS_HOST}:${REDIS_PORT}`)
     })
 
     redis.on('error', (err) => {
