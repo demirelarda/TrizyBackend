06774ba6804539ded86ef32932398a48de15e760
arda <yademirel21@gmail.com>
2025-01-28 18:52:44 +0300

    Updated password hashing system to use argon2

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,69 +1,79 @@
 const User = require("../models/User")
-const CryptoJS = require("crypto-js")
 const jwt = require("jsonwebtoken")
+const argon2 = require("argon2")
 const Subscription = require("../models/Subscription")
-const Like = require('../models/Like')
-const Cart = require('../models/Cart')
+const Like = require("../models/Like")
+const Cart = require("../models/Cart")
 const { createEmptyCartForUser } = require("../controllers/cartController")
 
+// CREATE USER
 exports.createUser = async (req, res) => {
+  try {
+    
+    const hashedPassword = await argon2.hash(req.body.password, {
+      type: argon2.argon2id,
+    })
+
     const newUser = new User({
       userFirstName: req.body.userFirstName,
       userLastName: req.body.userLastName,
       email: req.body.email,
-      password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET).toString(),
+      password: hashedPassword,
     })
-  
-    try {
-      const savedUser = await newUser.save()
-  
-      // create an empty cart for the user
-      const cartResult = await createEmptyCartForUser(savedUser._id)
-  
-      if (!cartResult.success) {
-        // if cart creation fails delete the user and return an error response
-        await User.findByIdAndDelete(savedUser._id)
-        return res.status(500).json({ success: false, message: "Signup failed: Unable to create user cart." })
-      }
-  
-      const accessToken = jwt.sign(
-        {
-          id: savedUser._id,
-          email: savedUser.email,
-          userFirstName: savedUser.userFirstName,
-          userLastName: savedUser.userLastName,
-          emailVerified: savedUser.emailVerified,
-        },
-        process.env.JWT_SEC //{ expiresIn: '45m' }
-      )
-  
-      const refreshToken = jwt.sign(
-        {
-          id: savedUser._id,
-        },
-        process.env.JWT_REFRESH_SEC //{ expiresIn: '7d' }
-      )
-  
-      savedUser.refreshToken = refreshToken
-      await savedUser.save()
-  
-      const { password, ...others } = savedUser._doc
-  
-      res.status(201).json({ ...others, isSubscriber: false, accessToken, refreshToken })
-    } catch (error) {
-      console.error("Error during user signup:", error)
-      res.status(500).json({ success: false, message: "Signup failed", error: error.message })
+
+    const savedUser = await newUser.save()
+
+    // Create an empty cart for the user
+    const cartResult = await createEmptyCartForUser(savedUser._id)
+    if (!cartResult.success) {
+      await User.findByIdAndDelete(savedUser._id)
+      return res.status(500).json({
+        success: false,
+        message: "Signup failed: Unable to create user cart.",
+      })
     }
+
+    const accessToken = jwt.sign(
+      {
+        id: savedUser._id,
+        email: savedUser.email,
+        userFirstName: savedUser.userFirstName,
+        userLastName: savedUser.userLastName,
+        emailVerified: savedUser.emailVerified,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "1h" }
+    )
+
+    const refreshToken = jwt.sign(
+      {
+        id: savedUser._id,
+      },
+      process.env.JWT_REFRESH_SEC,
+      { expiresIn: "7d" }
+    )
+
+    savedUser.refreshToken = refreshToken
+    await savedUser.save()
+
+    const { password, ...others } = savedUser._doc
+
+    res.status(201).json({ ...others, isSubscriber: false, accessToken, refreshToken })
+  } catch (error) {
+    console.error("Error during user signup:", error)
+    res.status(500).json({ success: false, message: "Signup failed", error: error.message })
+  }
 }
 
+// LOGIN USER
 exports.loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email })
     if (!user) return res.status(401).json("Wrong Login Details")
 
-    const decryptedPass = CryptoJS.AES.decrypt(user.password, process.env.SECRET)
-    const decryptedPassword = decryptedPass.toString(CryptoJS.enc.Utf8)
-    if (decryptedPassword !== req.body.password) {
+    // Verify the password with Argon2
+    const isPasswordValid = await argon2.verify(user.password, req.body.password)
+    if (!isPasswordValid) {
       return res.status(401).json("Wrong Login Details")
     }
 
@@ -85,7 +95,8 @@ exports.loginUser = async (req, res) => {
         userLastName: user.userLastName,
         emailVerified: user.emailVerified,
       },
-      process.env.JWT_SEC
+      process.env.JWT_SEC,
+      { expiresIn: "1h" }
     )
 
     let refreshToken
@@ -96,7 +107,7 @@ exports.loginUser = async (req, res) => {
       refreshToken = user.refreshToken
     } catch (error) {
       // If the token has expired, create a new refresh token
-      refreshToken = jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SEC)
+      refreshToken = jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SEC, { expiresIn: "7d" })
       user.refreshToken = refreshToken
       await user.save()
     }
@@ -125,123 +136,131 @@ exports.loginUser = async (req, res) => {
   }
 }
 
+// REFRESH TOKEN
 exports.refreshToken = async (req, res) => {
-    const refreshToken = req.body.token
-    if (!refreshToken) return res.status(403).json("Refresh token is required")
+  const { token } = req.body
 
-    try {
-        const user = await User.findOne({ refreshToken })
-        if (!user) return res.status(403).json("Invalid refresh token")
+  if (!token) {
+    return res.status(403).json("Refresh token is required")
+  }
 
-        jwt.verify(refreshToken, process.env.JWT_REFRESH_SEC, async (err, decoded) => {
-            if (err) return res.status(403).json("Invalid refresh token")
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_REFRESH_SEC)
+    const user = await User.findById(decoded.id)
 
-            const newAccessToken = jwt.sign({
-                id: user._id,
-                email: user.email,
-                userFirstName: user.userFirstName,
-                userLastName: user.userLastName,
-                emailVerified: user.emailVerified
-            }, process.env.JWT_SEC) //{ expiresIn: '45m' }
-
-            const newRefreshToken = jwt.sign({
-                id: user._id,
-            }, process.env.JWT_REFRESH_SEC) //{ expiresIn: '7d' }
-
-            user.refreshToken = newRefreshToken
-            await user.save()
-
-            res.status(200).json({ accessToken: newAccessToken, refreshToken: newRefreshToken })
-        })
-    } catch (error) {
-        res.status(500).json(error)
+    if (!user || user.refreshToken !== token) {
+      return res.status(403).json("Invalid refresh token")
     }
+
+    const newAccessToken = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SEC,
+      { expiresIn: "1h" }
+    )
+
+    const newRefreshToken = jwt.sign(
+      { id: user._id },
+      process.env.JWT_REFRESH_SEC,
+      { expiresIn: "7d" }
+    )
+
+    user.refreshToken = newRefreshToken
+    await user.save()
+
+    res.status(200).json({ accessToken: newAccessToken, refreshToken: newRefreshToken })
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error: error.message })
+  }
 }
 
+// LOGOUT USER
 exports.logoutUser = async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email })
-        if (!user) return res.status(403).json("User not found")
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(403).json("User not found")
 
-        user.refreshToken = ""
+    user.refreshToken = ""
+    await user.save()
+
+    res.status(200).json("User logged out")
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+// CHECK TOKENS
+exports.checkTokens = async (req, res) => {
+  const { accessToken, refreshToken } = req.body
+
+  if (!accessToken || !refreshToken) {
+    return res.status(400).json({
+      success: false,
+      message: "Access token and refresh token are required",
+    })
+  }
+
+  try {
+    jwt.verify(accessToken, process.env.JWT_SEC, async (err) => {
+      if (!err) {
+        return res.status(200).json({
+          success: true,
+          message: "Access token is still valid",
+          accessToken,
+          refreshToken,
+        })
+      }
+
+      const user = await User.findOne({ refreshToken })
+      if (!user) {
+        return res.status(403).json({
+          success: false,
+          message: "Invalid refresh token",
+        })
+      }
+
+      jwt.verify(refreshToken, process.env.JWT_REFRESH_SEC, async (err) => {
+        if (err) {
+          return res.status(403).json({
+            success: false,
+            message: "Invalid or expired refresh token",
+          })
+        }
+
+        const newAccessToken = jwt.sign(
+          {
+            id: user._id,
+            email: user.email,
+            userFirstName: user.userFirstName,
+            userLastName: user.userLastName,
+            emailVerified: user.emailVerified,
+          },
+          process.env.JWT_SEC,
+          { expiresIn: "1h" }
+        )
+
+        const newRefreshToken = jwt.sign(
+          { id: user._id },
+          process.env.JWT_REFRESH_SEC,
+          { expiresIn: "7d" }
+        )
+
+        user.refreshToken = newRefreshToken
         await user.save()
 
-        res.status(200).json("User logged out")
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
-
-exports.checkTokens = async (req, res) => {
-    const { accessToken, refreshToken } = req.body
-
-    if (!accessToken || !refreshToken) {
-        return res.status(400).json({
-            success: false,
-            message: "Access token and refresh token are required",
+        return res.status(200).json({
+          success: true,
+          message: "Access token refreshed",
+          accessToken: newAccessToken,
+          refreshToken: newRefreshToken,
         })
-    }
-
-    try {
-        jwt.verify(accessToken, process.env.JWT_SEC, async (err) => {
-            if (!err) {
-                return res.status(200).json({
-                    success: true,
-                    message: "Access token is still valid",
-                    accessToken,
-                    refreshToken,
-                })
-            }
-
-            const user = await User.findOne({ refreshToken })
-            if (!user) {
-                return res.status(403).json({
-                    success: false,
-                    message: "Invalid refresh token",
-                })
-            }
-
-            jwt.verify(refreshToken, process.env.JWT_REFRESH_SEC, async (err) => {
-                if (err) {
-                    return res.status(403).json({
-                        success: false,
-                        message: "Invalid or expired refresh token",
-                    })
-                }
-
-                const newAccessToken = jwt.sign(
-                    {
-                        id: user._id,
-                        email: user.email,
-                        userFirstName: user.userFirstName,
-                        userLastName: user.userLastName,
-                        emailVerified: user.emailVerified,
-                    },
-                    process.env.JWT_SEC
-                )
-
-                const newRefreshToken = jwt.sign(
-                    { id: user._id },
-                    process.env.JWT_REFRESH_SEC
-                )
-
-                user.refreshToken = newRefreshToken
-                await user.save()
-
-                return res.status(200).json({
-                    success: true,
-                    message: "Access token refreshed",
-                    accessToken: newAccessToken,
-                    refreshToken: newRefreshToken,
-                })
-            })
-        })
-    } catch (error) {
-        console.error("Error in checkTokens function:", error)
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server Error",
-            error: error.message,
-        })
-    }
+      })
+    })
+  } catch (error) {
+    console.error("Error in checkTokens function:", error)
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    })
+  }
 }
